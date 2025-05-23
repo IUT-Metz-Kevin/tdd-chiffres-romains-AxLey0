2164cc416b837e79bd5d41316ee2a4a538f3eecc
AxLey <alexglay57@hotmail.com>
2025-05-23 11:44:32 +0200

    Suppresion et modification des fichiers

--- dist/src/roman-numerals.test.js
+++ dist/src/roman-numerals.test.js
@@ -1,10 +1,30 @@
 import { expect, test } from "vitest";
-import { romanToInt } from "./roman-numerals.js";
+export class RomanNumeral {
+    romanToInt(value) {
+        const romanNumerals = {
+            'I': 1,
+        };
+        let total = 0;
+        let prevValue = 0;
+        for (let i = value.length - 1; i >= 0; i--) {
+            const currentValue = romanNumerals[value[i]];
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            }
+            else {
+                total += currentValue;
+            }
+            prevValue = currentValue;
+        }
+        return total;
+    }
+}
 test.each([
     ["I", 1],
     ["II", 2],
     ["III", 3],
 ])("Roman numeral %s should be %d", (roman, expected) => {
-    expect(romanToInt(roman)).toBe(expected);
+    const romanNumeral = new RomanNumeral();
+    expect(romanNumeral.romanToInt(roman)).toBe(expected);
 });
 //# sourceMappingURL=roman-numerals.test.js.map
