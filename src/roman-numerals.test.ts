3764fc20573a0a2b23980eb22491e6192fec5c18
AxLey <alexglay57@hotmail.com>
2025-05-23 11:24:38 +0200

    Correction du fichier roman-numerals.test.ts

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,1 +1,10 @@
 // Write your test here
+import { expect, test } from "vitest";
+
+test.each([
+    ["I", 1],
+    ["II", 2],
+    ["III", 3],
+    ])("Roman numeral %s should be %d", (roman, expected) => {
+    expect(romanToInt(roman)).toBe(expected);
+    });
